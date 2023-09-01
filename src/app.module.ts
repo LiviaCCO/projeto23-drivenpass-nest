59432e019508b2898a04bb58131f1a867612c0b1
LiviaCCO <livia.ccapistrano@gmail.com>
2023-09-01 15:52:09 -0300

    versão inicial

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
+import { UsersModule } from './users/users.module';
+import { CredentialsModule } from './credentials/credentials.module';
+import { NotesModule } from './notes/notes.module';
+import { CardsModule } from './cards/cards.module';
+import { EraseModule } from './erase/erase.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [HealthModule, UsersModule, CredentialsModule, NotesModule, CardsModule, EraseModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
